081a61d573d749ba6e674cbccf8b9b5ce0cb76ab
demershow <demersontorres520@gmail.com>
2022-08-22 11:23:36 -0300

    init rep and add mysql conn (i've been 2 hours to connect this)

---
+++ inc/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  database: 'saboroso',
+  password: '',
+});
+
+module.exports = connection;
