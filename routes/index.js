4a137cb06463e9f9e73adb96372827a247b42a4e
demershow <demersontorres520@gmail.com>
2022-08-23 08:39:21 -0300

    MY10: Refactoring the code and turning every element in to a component

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,18 @@
 var conn = require ('./../inc/db');
 var express = require('express');
 var router = express.Router();
+var menus = require('./../inc/menus')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results)=>{
-    if(err){
-      console.log(err);
-    }
+  menus.getMenus().then(results=>{
     res.render('index.ejs', {
-       title: 'Restaurante Saboroso!',
-       menus: results,
-       background: 'images/img_bg_1.jpg'
-      });
+      title: 'Restaurante Saboroso!',
+      menus: results,
+      background: 'images/img_bg_1.jpg',
+      isHome:true
+     });
   })
 
   
@@ -26,25 +23,33 @@ router.get('/contacts', function(req, res, next){
   res.render('contacts',{
     title: 'contato - Restaurante Saboroso!',
     background: 'images/img_bg_3.jpg',
-    h1: "Diga OI!"
+    h1: "Diga OI!",
+
   })
 
 })
 router.get('/menus', function(req, res, next){
 
-  res.render('menus', {
-    title: 'Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1: "Saboreie os nossos menus!"
+  menus.getMenus().then(results =>{
+    res.render('menus', {
+      title: 'Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: "Saboreie os nossos menus!",
+      menus:results,
+
+    })
+  
   })
 
+ 
 })
 router.get('/reservations', function(req, res, next){
 
   res.render('reservations', {
     title: 'Restaurante Saboroso!',
     background: 'images/img_bg_2.jpg',
-    h1: "Reserve uma mesa!"
+    h1: "Reserve uma mesa!",
+
   })
 
 })
@@ -53,7 +58,8 @@ router.get('/services', function(req, res, next){
   res.render('services', {
     title: 'Restaurante Saboroso!',
     background: 'images/img_bg_1.jpg',
-    h1: "Conheça nossos serviços!"
+    h1: "Conheça nossos serviços!",
+
   })
 
 })
